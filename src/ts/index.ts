4ff26389b68d2abba1d847191725372fea9f0b64
Luis Sanchez Alcazar <luis.sa1973@gmail.com>
2020-09-14 14:51:46 +0200

    creation of classes shorcuts

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -5,89 +5,24 @@ const {log:l}=console;
 
 cL("Hola a todos!!!","orange");
 
-class Empleado {
 
-    [k:string]:any;
-
-    constructor(
-        public nombre:string,
-        public posicion:string,
-        private  salario:number,
-        ) {
-
-            this.nombre=nombre;
-            this.posicion=posicion;
-            this.salario=salario;
-
-    }
-
-    getSalario ():number {
-        return this.salario
-    }
-
-    setSalario(v:number) {
-        this.salario=v
-    }
-
-    accept(visitor:any) {
-            visitor.visit(this)
-
-    }
+class MyClass3 {
+  public constructor(public p1: number, public p2: string) {}
 }
 
-interface pot {
-    // [k:string]:any;
-    nuevaProperty:any;        
+class Person {
+
+  constructor(readonly nombre: string, readonly edad: number) {
+
+  }
 }
+const myClass3 = new MyClass3(1, "2");
+console.log(myClass3.p1);
 
-interface Emp {
-    setSalario(d:number):void;
-    getSalario():number;
-    // [k:string]:any;
-}
+let luis:Person;
 
-type Comp=Emp & pot
+luis=new Person("Luis",47);
 
-class AumentaSalario {
-    visit<T extends Comp>(emp:T) {
-
-        emp.setSalario( emp.getSalario() *1.5)
-
-        // emp.Salario=emp.Salario * 1.5
-
-    }
-};
-class DisminuyeSalario {
-    visit<T extends Comp>(emp:T) {
-
-        emp.setSalario( emp.getSalario() * 0.5)
-        emp.nuevaProperty=23
-
-        // emp.Salario=emp.Salario * 1.5
-
-    }
-};
-
-let Empleados=[
-    new Empleado("Luis","FrontEnd Developper",22000),
-    new Empleado("Ana","UI Design",19000),
-    
-];
-
-cO(Empleados,"orange",3)
-
-for(let empl of Empleados) {
-    empl.accept(new AumentaSalario())
-};
-
-
-cO(Empleados,"cyan",3)
-
-
-for(let empl of Empleados) {
-    empl.accept(new DisminuyeSalario())
-};
-
-
-cO(Empleados,"gray",3)
-
+cO(luis,"red",3);
+// luis.nombre="Pepe"
+cL(luis.nombre,"magenta")
