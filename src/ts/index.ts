c17659160d65db16582cd60a8ef09f3d14d563cb
Luis Sanchez Alcazar <luis.sa1973@gmail.com>
2020-09-14 14:52:31 +0200

    static and abstract

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -5,24 +5,12 @@ const {log:l}=console;
 
 cL("Hola a todos!!!","orange");
 
-
-class MyClass3 {
-  public constructor(public p1: number, public p2: string) {}
+abstract class FakeStaticClass {
+ public static m1: number;
+ public static f1(): void { }
 }
 
-class Person {
+console.log(FakeStaticClass.m1);
+FakeStaticClass.f1();
 
-  constructor(readonly nombre: string, readonly edad: number) {
-
-  }
-}
-const myClass3 = new MyClass3(1, "2");
-console.log(myClass3.p1);
-
-let luis:Person;
-
-luis=new Person("Luis",47);
-
-cO(luis,"red",3);
-// luis.nombre="Pepe"
-cL(luis.nombre,"magenta")
+// const instance1 = new FakeStaticClass(); // Doesn't compile
